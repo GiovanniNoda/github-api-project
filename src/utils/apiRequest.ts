f6091d530df7a479871b15f29351c02d018f4075
Giovanni <giovanniminoru@gmail.com>
2025-04-15 22:32:53 -0300

    Update apiRequest.ts

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -3,28 +3,28 @@ import { ReposProps } from "../utils/types"
 
 export async function getApiUserGithub(username: string): Promise<UserProps | null> {
     try {
-        const response = await fetch(`https://api.github.com/users/${username}`);
+        const response = await fetch(`https://api.github.com/users/${username}`)
 
         // Verificando o status 403 (requisição negada)
         if (response.status === 403) {
-            alert('Requisição negada. Limite de requisições atingido.');
-            console.error('Erro 403: Limite de requisições atingido. Tente novamente mais tarde.');
-            return null;
+            alert('Requisição negada. Limite de requisições atingido.')
+            console.error('Erro 403: Limite de requisições atingido. Tente novamente mais tarde.')
+            return null
         }
 
         // Verificando o status 404 (usuário não encontrado)
         if (response.status === 404) {
-            alert('Usuário não encontrado');
-            console.error('Erro 404: Usuário não encontrado');
+            alert('Usuário não encontrado')
+            console.error('Erro 404: Usuário não encontrado')
             return null;
         }
 
         // Caso o status seja diferente de 200 (OK), lança um erro genérico
         if (!response.ok) {
-            throw new Error('Erro desconhecido');
+            throw new Error('Erro desconhecido')
         }
 
-        const data = await response.json();
+        const data = await response.json()
 
         // Verifique se os dados realmente contêm as propriedades esperadas
         if (!data || !data.login || !data.name || !data.avatar_url) {
@@ -43,7 +43,7 @@ export async function getApiUserGithub(username: string): Promise<UserProps | nu
         return userData;
 
     } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido';
+        const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido'
         alert(errorMessage);
         return null;
     }
@@ -51,7 +51,7 @@ export async function getApiUserGithub(username: string): Promise<UserProps | nu
 
 export async function getApiUserGithubRepos(username: string): Promise<ReposProps[] | null> {
     try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+        const response = await fetch(`https://api.github.com/users/${username}/repos`)
 
         if (!response.ok) {
             throw new Error('Erro desconhecido')
@@ -61,7 +61,7 @@ export async function getApiUserGithubRepos(username: string): Promise<ReposProp
         const data = await response.json();
 
         if (!Array.isArray(data) || data.length === 0) {
-            throw new Error('Não há repositórios.');
+            throw new Error('Não há repositórios.')
         }
 
         const reposData: ReposProps[] = data.map(repo => ({
@@ -74,7 +74,7 @@ export async function getApiUserGithubRepos(username: string): Promise<ReposProp
         return reposData;
 
     } catch (error) {
-        console.error(error);
-        return null;
+        console.error(error)
+        return null
     }
 }
